66b149f1b44226c114c1ec1ffeec8c13e27dcd4f
esuau <evan.suau@etu.u-pec.fr>
2019-02-20 23:58:27 +0100

    refactor(read): Manage case when no connection is found

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -9,15 +9,18 @@ object Controller {
 
   def processFile(filename: String): Unit = {
     val lastHourConnections: List[Connection] = Service.getHourConnections(Source.fromFile(filename))
-    println("Last hour connections:")
-    lastHourConnections.foreach(println)
 
     if (lastHourConnections.nonEmpty) {
+      println("Last hour connections:")
+      lastHourConnections.foreach(println)
+
       val mostActive = Service.getMostActiveServer(lastHourConnections)
       println(s"Most active server: ${mostActive._1} with ${mostActive._2} initiated connections.")
 
       val mostRecentConnection = Service.getMostRecentConnection(lastHourConnections)
       println(s"Most recent connection: ${mostRecentConnection.sourceHost} to ${mostRecentConnection.targetHost} on ${mostRecentConnection.time}")
+    } else {
+      println("No connection performed during the last hour.")
     }
   }
 
