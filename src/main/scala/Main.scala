9c1eb2c757bd9e94bf88d440f9be18335fbf6b63
esuau <evan.suau@etu.u-pec.fr>
2019-02-21 00:06:21 +0100

    feat(schedule): Trigger program every hour

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,16 @@
+import java.util.{Timer, TimerTask}
 import controller.Controller
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    Controller.processFile(args(0))
+    val timer = new Timer()
+    val hourlyTask = new TimerTask() {
+      override def run(): Unit = {
+        Controller.processFile(args(0))
+      }
+    }
+    timer.schedule(hourlyTask, 1, 3600000)
   }
 
 }
